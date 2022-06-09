e3fea4a409abe6d87e6396ed8dc25379cd57244d
Kyi Htoo Aung <kyihtooag@gmail.com>
2022-06-09 21:49:43 +0630

    added tailwindcss and dart_sass

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Aria.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1.5"}
+      {:tailwind, "~> 0.1.5"},
+      {:dart_sass, "~> 0.5.0"}
     ]
   end
 
@@ -61,11 +62,21 @@ defmodule Aria.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      "assets.build": [
+        "esbuild default",
+        "sass default",
+        "tailwind default"
+      ],
+      setup: ["deps.get", "ecto.setup",  "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": [
+        "esbuild default --minify",
+        "sass default",
+        "tailwind default --minify",
+        "phx.digest"
+      ]
     ]
   end
 end
